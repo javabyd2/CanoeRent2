777eaf055115dc3e747321611b4c8abfa28bda0f
Filip <filipmel@gmail.com>
2018-06-10 13:45:49 +0200

    added Trip model

--- src/main/java/com/canoerent/model/Trip.java
+++ src/main/java/com/canoerent/model/Trip.java
@@ -1,4 +1,75 @@
 package com.canoerent.model;
 
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "trips")
 public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "trip_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String location;
+
+    @Column(unique = true)
+    private String startDate;
+
+    @Column(unique = true)
+    private String endDate;
+
+    @ManyToMany(mappedBy = "tripSet",cascade = CascadeType.ALL)
+
+    Set<User> userSet = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public Set<User> getUserSet() {
+        return userSet;
+    }
+
+    public void setUserSet(Set<User> userSet) {
+        this.userSet = userSet;
+    }
 }
+
+
+
+
+
+
+
+
